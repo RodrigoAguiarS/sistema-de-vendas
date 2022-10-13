0c820c4c4958b5cc7f599d98d281160eabd6da6f
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-10-13 18:11:31 -0300

    iniciando projeto, entidades mapeadas

---
+++ src/main/java/com/rodrigo/vendas/modelo/ItemPedido.java
@@ -0,0 +1,41 @@
+package com.rodrigo.vendas.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "item_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido")
+    @JsonIgnore
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+
+    @Column
+    private Integer quantidade;
+
+}
---
+++ src/main/java/com/rodrigo/vendas/modelo/enums/StatusPedido.java
@@ -0,0 +1,8 @@
+package com.rodrigo.vendas.modelo.enums;
+
+public enum StatusPedido {
+    
+    REALIZADO,
+    CANCELADO;
+}
+
